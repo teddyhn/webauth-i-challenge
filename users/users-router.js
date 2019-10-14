3b8e4fbedd0c95c6f1811c861a45c23392ba1512
teddyhn <tngo97@gmail.com>
2019-10-14 12:14:20 -0700

    Register endpoint functional

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model.js');
 
 const router = express.Router();
+
+router.post('/register', (req, res) => {
+    let { username, password } = req.body;
+
+    if (username && password) {
+        const hash = bcrypt.hashSync(password, 10);
+    
+        password = hash;
+    
+        Users.add(req.body)
+            .then(saved => {
+                res.status(201).json(saved)
+            })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+    } else {
+        res.status(400).json({ message: "Please provide valid credentials" })
+    }
+});
 
 module.exports = router;
